9d5ced794c918720488c636da7bfc8ad3f328645
Mattia Berrueta <berrueta.mattia@gmail.com>
2022-06-12 12:19:08 +0200

    feat: navigation structure

---
+++ lib/_core/navigation.dart
@@ -0,0 +1,15 @@
+import 'package:auto_route/annotations.dart';
+import 'package:injectable/injectable.dart';
+import 'package:readee/screens/detail_screen.dart';
+import 'package:readee/screens/home_screen.dart';
+
+@MaterialAutoRouter(
+  replaceInRouteName: 'Screen',
+  routes: <AutoRoute>[
+    AutoRoute(page: HomeScreen, initial: true),
+    AutoRoute(page: DetailScreen),
+  ],
+)
+
+@singleton
+class $AppRouter {}
