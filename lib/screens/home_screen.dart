1805f4778762ed8966d7927d85e69871576bca8f
Mattia Berrueta <berrueta.mattia@gmail.com>
2022-06-13 18:49:58 +0200

    feat: updated home screen with real data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,23 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:readee/_core/navigation.gr.dart';
+import 'package:readee/_domain/book_model.dart';
+import 'package:readee/depencency_injection.dart';
+import 'package:readee/_domain/book_list_cubit.dart';
 import 'package:readee/screens/filter_modal.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatefulWidget implements AutoRouteWrapper {
   const HomeScreen({super.key});
 
   @override
   State<StatefulWidget> createState() => HomeState();
+
+  @override
+  Widget wrappedRoute(BuildContext context) => BlocProvider(
+        create: (_) => getIt<BookCubit>(),
+        child: this,
+      );
 }
 
 class HomeState extends State<HomeScreen> {
@@ -15,15 +25,53 @@ class HomeState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Readee")),
-      body: ListView.builder(
-        itemBuilder: (context, index) => GestureDetector(
-          onTap: () => context.router.push(const DetailScreenRoute()),
-          child: ListTile(title: Text("Book n. $index")),
-        ),
+      body: BlocBuilder<BookCubit, List<BookModel>>(
+        builder: ((_, items) {
+          return ListView.separated(
+            itemCount: items.length,
+            separatorBuilder: (_, __) => const Divider(),
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () => context.router.push(const DetailScreenRoute()),
+                child: _BookListTile(book: items[index]),
+              );
+            },
+          );
+        }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => showFilterModal(context),
         child: const Icon(Icons.search),
+      ),
+    );
+  }
+}
+
+class _BookListTile extends StatelessWidget {
+  final BookModel book;
+
+  const _BookListTile({required this.book});
+
+  @override
+  Widget build(BuildContext context) {
+    final bookAvailable = book.copies > 0;
+    final color =
+        bookAvailable ? Colors.green : Colors.orange;
+    final icon =
+        bookAvailable ? Icons.check_circle_outline : Icons.access_time_outlined;
+
+    return ListTile(
+      title: Text(book.name),
+      subtitle: Text(book.author),
+      leading: Container(
+        height: 30,
+        width: 30,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(30)),
+          color: color,
+        ),
+        alignment: Alignment.center,
+        child: Icon(icon, color: Colors.white),
       ),
     );
   }
