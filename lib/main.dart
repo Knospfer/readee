8421d2b6c05f2120d72261c88d3bd91c074c5102
Mattia Berrueta <berrueta.mattia@gmail.com>
2022-06-13 18:48:56 +0200

    feat: di engine

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:readee/_core/navigation.gr.dart';
+import 'package:readee/depencency_injection.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   final _router = AppRouter();
-
   MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp.router(
       title: 'Readee',
       theme: ThemeData(primarySwatch: Colors.blue),
