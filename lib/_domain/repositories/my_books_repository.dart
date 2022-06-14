91829e8086cc730698a8b851a83de11616c03164
Mattia Berrueta <berrueta.mattia@gmail.com>
2022-06-14 19:34:25 +0200

    fix: update book and delete book witk right id

--- lib/_domain/repositories/my_books_repository.dart
+++ lib/_domain/repositories/my_books_repository.dart
@@ -6,16 +6,21 @@ import 'package:readee/_domain/models/book_model.dart';
 @lazySingleton
 class MyBookRepository {
   final CollectionReference _collection =
-  FirebaseFirestore.instance.collection('my_books');
+      FirebaseFirestore.instance.collection('my_books');
 
   Future<BookModel?> getBook(String bookId) async {
-    final querySnapshotList =
-    await _collection.where("id", isEqualTo: bookId).get();
-    final querySnapshot = querySnapshotList.docs.firstOrNull;
+    final querySnapshot = await _getSnapshow(bookId);
     if (querySnapshot == null) return null;
     final bookRaw = querySnapshot.data() as Map<String, dynamic>;
 
     return BookModel.fromJson(bookRaw);
+  }
+
+  Future<QueryDocumentSnapshot?> _getSnapshow(String bookId) async {
+    final querySnapshotList =
+        await _collection.where("id", isEqualTo: bookId).get();
+
+    return querySnapshotList.docs.firstOrNull;
   }
 
   Future<void> upsertBook(BookModel bookModel) async {
@@ -26,9 +31,15 @@ class MyBookRepository {
   Future<DocumentReference> _addBook(BookModel bookModel) =>
       _collection.add(bookModel.toJson());
 
-  Future<void> _updateBook(BookModel bookModel) =>
-      _collection.doc(bookModel.id).update(bookModel.toJson());
+  Future<void> _updateBook(BookModel bookModel) async {
+    final snapshot = await _getSnapshow(bookModel.id);
+    if (snapshot == null) return;
+    await _collection.doc(snapshot.id).update(bookModel.toJson());
+  }
 
-  Future<void> removeBook(BookModel bookModel) =>
-      _collection.doc(bookModel.id).delete();
+  Future<void> removeBook(BookModel bookModel) async {
+    final snapshot = await _getSnapshow(bookModel.id);
+    if (snapshot == null) return;
+    await _collection.doc(snapshot.id).delete();
+  }
 }
