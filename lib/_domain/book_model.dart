0dd128c4917fea6363f3bba10b2d8ded0303d8b5
Mattia Berrueta <berrueta.mattia@gmail.com>
2022-06-14 16:15:30 +0200

    feat: added freezed to book model

--- lib/_domain/book_model.dart
+++ lib/_domain/book_model.dart
@@ -1,22 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:json_annotation/json_annotation.dart';
+
+part 'book_model.freezed.dart';
 
 part 'book_model.g.dart';
 
-//TODO FREEZED COPY WITH
-@immutable
 @JsonSerializable()
-class BookModel {
-  final String id;
-  final String name;
-  final String author;
-  final String genre;
-  final int copies;
-  @TimestampConverter()
-  final DateTime? date;
-
-  bool get isAvailable => copies > 0;
+@freezed
+class BookModel
+    with _$BookModel {
 
   ///The Database is designed to be used by only one user
   ///(there is no user table), so in this particular case
@@ -25,26 +19,27 @@ class BookModel {
 
   int? get daysRemaining {
     final acutalDate = date;
-    if(acutalDate == null) return null;
-
+    if (acutalDate == null) return null;
     final range = DateTimeRange(start: DateTime.now(), end: acutalDate);
 
     return range.duration.inDays;
   }
 
-  const BookModel(
-    this.id,
-    this.name,
-    this.author,
-    this.genre,
-    this.copies,
-    this.date,
-  );
+  bool get isAvailable => copies > 0;
+
+  const factory BookModel({
+    required String id,
+    required String name,
+    required String author,
+    required String genre,
+    required int copies,
+    @TimestampConverter() DateTime? date,
+  }) = _BookModel;
 
   factory BookModel.fromJson(Map<String, dynamic> json) =>
       _$BookModelFromJson(json);
 
-  Map<String, dynamic> toJson() => _$BookModelToJson(this);
+  const BookModel._();
 }
 
 class TimestampConverter extends JsonConverter<DateTime?, Timestamp?> {
